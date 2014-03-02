9631b12ccf5f44a91bab9cedc7b207eb3e2f2c46
nsrose <nsrose@berkeley.edu>
2014-03-01 22:26:54 -0800

    fixed number bug

--- appengine_files/logic.py
+++ appengine_files/logic.py
@@ -5,13 +5,16 @@ def extract_words(string):
 	return string.split() 
 
 def remove_letters(word):
-	result = word
-	if len(word)>5:
-		index = random.randint(1, len(word)-2)
-		if not result[len(result)-1] in ascii_letters:
-			index -= 1 
-		result = result[:index] + result[index+1:]
-	return result
+	if not word.isalpha():
+		return word
+	else:
+		result = word
+		if len(word)>5:
+			index = random.randint(1, len(word)-2)
+			if not result[len(result)-1] in ascii_letters:
+				index -= 1 
+			result = result[:index] + result[index+1:]
+		return result
 
 def mutate_all(submission):
 	new_words_list = map(remove_letters, extract_words(submission))
